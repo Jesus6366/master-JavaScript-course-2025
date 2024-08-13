a295aee379d6c53a3cb23890ff7f593c35884b20
unknown <polibos6366@gmail.com>
2024-08-12 20:34:29 -0600

    optional chaining

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -28,6 +28,25 @@ const restaurant = {
   },
 };
 
+// OPTIONAL CHAINING use to avoid error if a property does not exist
+console.log(restaurant.openingHours.mon?.open);
+console.log(restaurant.openingHours?.mon?.open);
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+for (const day of days) {
+  // console.log(day);
+  const open = restaurant.openingHours[day]?.open ?? 'closed';
+  console.log(`On ${day}, we open at ${open}`);
+}
+
+// on methods
+console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// on arrays
+const usersS = [{ name: 'jesus', age: 32 }];
+console.log(usersS[0]?.name ?? 'User array empty');
+
 // THE FOR OF LOOP
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
