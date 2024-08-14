81e11202be6adaec8faff646165589af37ae2eb6
unknown <polibos6366@gmail.com>
2024-08-13 19:28:51 -0600

    Default values in functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bookings = [];
+
+// default values
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', undefined, 1200);
