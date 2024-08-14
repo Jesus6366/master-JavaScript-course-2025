1e69a4882768b665c782e6cd0df7e7c592d7531d
unknown <polibos6366@gmail.com>
2024-08-13 19:45:01 -0600

    value vs reference

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,22 +1,46 @@
 'use strict';
 
-const bookings = [];
+// const bookings = [];
 
-// default values
-const createBooking = function (flightNum, numPassengers = 1, price = 199) {
-  // ES5
-  //   numPassengers = numPassengers || 1;
-  //   price = price || 199;
+// // default values
+// const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+//   // ES5
+//   //   numPassengers = numPassengers || 1;
+//   //   price = price || 199;
 
-  const booking = {
-    flightNum,
-    numPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   //   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', undefined, 1200);
+
+// value vs reference
+
+const flight = 'LH234';
+
+const jesus = {
+  name: 'jesus martha',
+  passport: 32135245646,
 };
 
-createBooking('LH123');
-createBooking('LH123', 2, 800);
-createBooking('LH123', undefined, 1200);
+const checkIn = function (flightNum, passenger) {
+  // this will modify the original jesus object because it is of reference type
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 32135245646) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jesus);
+console.log(flight);
+console.log(jesus);
