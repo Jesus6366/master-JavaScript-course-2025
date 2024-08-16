c6bf2fd612c1806e6d4f4e55ceab34c307a331bc
Jesus Jose Martha Laborin <89470786+Jesus6366@users.noreply.github.com>
2024-08-16 16:29:15 -0600

    reminder operator

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -266,3 +266,34 @@ btnSort.addEventListener('click', function (e) {
 
 // console.log(Number.isFinite(20));
 // console.log(Number.isFinite('20'));
+
+// MAth
+
+// console.log(Math.sqrt(25));
+// console.log(Math.max(25, '23', 20, 10, 33));
+// console.log(Math.min(25, '23', 20, 10, 33));
+// console.log(Math.PI);
+
+// // random numbers
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (min - max) + 1) + min;
+// console.log(randomInt(50, 1000));
+
+//ROUNDING INT
+// // removes al decimals
+// console.log(Math.trunc(23.3));
+// // rounds to the nearest
+// console.log(Math.round(23.9));
+// // rounds up
+// console.log(Math.ceil('23,4'));
+// // rounds down this is better than trunc
+// console.log(Math.floor('23,4'));
+// // adding decimals
+// console.log((23).toFixed(4));
+
+// // THE REMINDER OPERATOR
+// console.log(5 % 2);
+
+// const isEven = n => n % 2 === 0;
+// console.log(isEven(8));
+// console.log(isEven(23));
