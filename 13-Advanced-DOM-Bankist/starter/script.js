9a2f7231e66aa6638c67f86f3ee7e72d5c0cf827
unknown <polibos6366@gmail.com>
2024-08-18 10:29:19 -0600

    selecting creating and deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,52 @@ document.addEventListener("keydown", function (e) {
     closeModal();
   }
 });
+
+//////////////////////////////////////////
+//SELECTING ELEMENTS
+// console.log(document.documentElement);
+// console.log(document.head);
+// console.log(document.body);
+
+// the most commonly used
+const header = document.querySelector(".header");
+const allSections = document.querySelectorAll(".section");
+// console.log(allSections);
+
+document.getElementById("section--1");
+const allButtons = document.getElementsByTagName("button");
+// console.log(allButtons);
+
+document.getElementsByClassName("btn");
+//////////////////////////////////////////
+// CREATIONG AND INSERTING ELEMENTS
+//.insertAdjacentHTML
+
+// element created but not yet inserted into the page
+const message = document.createElement("div");
+message.classList.add("cookie-message");
+// message.textContent =
+//   "We use cookies for improved functionality and analytics.";
+message.innerHTML =
+  "We use cookies for improved functionality and analytics. <button class='btn btn--close-cookie'>Got it!</button>";
+
+// this will not appered until we clone it
+// header.append(message.cloneNode(true));
+
+// inserting before or after
+// header.before(message)
+
+// Append the message to the header
+header.append(message);
+
+// DELETE ELEMENTS
+// Add the event listener after the message has been appended to the DOM
+message
+  .querySelector(".btn--close-cookie")
+  .addEventListener("click", function () {
+    // THIS METHOD IS NEW
+    // message.remove();
+
+    // THIS THE OLD WAY
+    message.parentElement.removeChild(message);
+  });
