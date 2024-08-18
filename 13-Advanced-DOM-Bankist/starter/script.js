e9cf473b4f0de0021a193a8ed044bbc76a6c9a5b
unknown <polibos6366@gmail.com>
2024-08-18 16:31:43 -0600

    passing arguments to event handlers

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -7,6 +7,12 @@ const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
+const nav = document.querySelector(".nav");
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+
+const tabsContent = document.querySelectorAll(".operations__content");
+
 ///////////////////////////////////////
 // Modal window
 
@@ -77,10 +83,6 @@ document.querySelector(".nav__links").addEventListener("click", (e) => {
 });
 
 // TAB COMPONENTS
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-
-const tabsContent = document.querySelectorAll(".operations__content");
 
 tabsContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".operations__tab");
@@ -102,6 +104,29 @@ tabsContainer.addEventListener("click", function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add("operations__content--active");
 });
+
+// refactoring
+const handleHover = function (e, opacity) {
+  if (e.target.classList.contains("nav__link")) {
+    const link = e.target;
+    const sibling = link.closest(".nav").querySelectorAll(".nav__link");
+    const logo = link.closest(".nav").querySelector("img");
+
+    sibling.forEach((el) => {
+      if (el !== link) {
+        return (el.style.opacity = this);
+      }
+      logo.style.opacity = this;
+    });
+  }
+};
+
+//// MENU FADE ANIMATION
+//event delegation again
+// with mouseover it bubbles
+nav.addEventListener("mouseover", handleHover.bind(0.5));
+
+nav.addEventListener("mouseout", handleHover.bind(1));
 
 //////////////////////////////////////////
 //SELECTING ELEMENTS
