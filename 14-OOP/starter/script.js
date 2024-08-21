19b0047c9d902c9cc4dccb8e1662eb1bea9a2d88
unknown <polibos6366@gmail.com>
2024-08-20 19:42:10 -0600

    constructor functions and the new operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+// CONSTRUCTOR FUNCTION
+// always start with capital letters
+// an arrow function wont work
+const Person = function (firstName, birthYear) {
+  // instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //methods never created like this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const jesus = new Person("Jesus", 1990);
+console.log(jesus);
+
+//1. New {} is created
+//2. function is calles, this = {}
+//3. {} is linked to prototype
+//4. functions automatically return {}
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+
+console.log(jesus instanceof Person);
