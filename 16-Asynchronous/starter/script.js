7dd8b88f447ae08b83225576074d5f611cf604a5
unknown <polibos6366@gmail.com>
2024-08-24 12:09:31 -0600

    handling rejected promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -3,6 +3,33 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
+const renderError = function (message) {
+  countriesContainer.insertAdjacentText("beforeend", message);
+  //   countriesContainer.style.opacity = 1;
+};
+
+const renderCountry = function (data, className = "") {
+  const html = `<article class="country ${className}" >
+              <img class="country__img" src="${data.flag}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(
+                  +data.population / 1000000
+                ).toFixed(1)}</p>
+                <p class="country__row"><span>🗣️</span>${
+                  data.languages[0].name
+                }</p>
+                <p class="country__row"><span>💰</span>${
+                  data.currencies[0].name
+                }</p>
+              </div>
+            </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeEnd", html);
+  //   countriesContainer.style.opacity = 1;
+};
+
 ///////////////////////////////////////
 ////////////////OLD SCHOOL way without promises
 
@@ -42,28 +69,6 @@ const countriesContainer = document.querySelector(".countries");
 // getCountryData("mexico");
 // getCountryData("portugal");
 // getCountryData("usa");
-
-const renderCountry = function (data, className = "") {
-  const html = `<article class="country ${className}" >
-            <img class="country__img" src="${data.flag}" />
-            <div class="country__data">
-              <h3 class="country__name">${data.name}</h3>
-              <h4 class="country__region">${data.region}</h4>
-              <p class="country__row"><span>👫</span>${(
-                +data.population / 1000000
-              ).toFixed(1)}</p>
-              <p class="country__row"><span>🗣️</span>${
-                data.languages[0].name
-              }</p>
-              <p class="country__row"><span>💰</span>${
-                data.currencies[0].name
-              }</p>
-            </div>
-          </article>`;
-
-  countriesContainer.insertAdjacentHTML("beforeEnd", html);
-  countriesContainer.style.opacity = 1;
-};
 
 // const getCountryAndNeighbour = function (country) {
 //   // ajax call country 1
@@ -150,7 +155,17 @@ const getCountryData = function (country) {
       )
         .then((response) => response.json())
         .then((data) => renderCountry(data, "neighbour"));
+    })
+    .catch((err) => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
     });
 };
+btn.addEventListener("click", function () {
+  getCountryData("mexico");
+});
 
-getCountryData("mexico");
+getCountryData("hashahshs");
