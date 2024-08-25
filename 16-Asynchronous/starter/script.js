6a9b4db80fb4d6955ebe09be752344d7c5819cbb
unknown <polibos6366@gmail.com>
2024-08-25 09:40:36 -0600

    promisifying the geolocation API

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -282,34 +282,46 @@ GOOD LUCK 😀
 
 /// builgind a simple promise
 // the funcions is the executer
-const lotteryPromise = new Promise(function (resolve, reject) {
-  console.log("Lotter draw is happening");
+// const lotteryPromise = new Promise(function (resolve, reject) {
+//   console.log("Lotter draw is happening");
 
-  setTimeout(function () {
-    if (Math.random() >= 0.5) {
-      // the response
-      resolve("You WIN 🎉");
-    } else {
-      // the error response
-      reject(new Error("You lost your money "));
-    }
-  }, 2000);
-});
+//   setTimeout(function () {
+//     if (Math.random() >= 0.5) {
+//       // the response
+//       resolve("You WIN 🎉");
+//     } else {
+//       // the error response
+//       reject(new Error("You lost your money "));
+//     }
+//   }, 2000);
+// });
 
-lotteryPromise
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+// lotteryPromise
+//   .then((res) => console.log(res))
+//   .catch((err) => console.error(err));
 
-// promisifyin setTimeout
-const wait = function (seconds) {
-  return new Promise(function (resolve) {
-    setTimeout(resolve, seconds * 1000);
+// // promisifyin setTimeout
+// const wait = function (seconds) {
+//   return new Promise(function (resolve) {
+//     setTimeout(resolve, seconds * 1000);
+//   });
+// };
+
+// wait(2)
+//   .then(() => {
+//     console.log("I waited for 2 seconds ");
+//     return wait(1);
+//   })
+//   .then(() => "I waited for 1 second");
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => resolve(position),
+    //   (err) => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
   });
 };
 
-wait(2)
-  .then(() => {
-    console.log("I waited for 2 seconds ");
-    return wait(1);
-  })
-  .then(() => "I waited for 1 second");
+getPosition().then((pos) => console.log(pos));
