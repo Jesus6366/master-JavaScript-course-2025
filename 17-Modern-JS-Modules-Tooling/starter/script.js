03838b33ce095323471c6e52136e617f696edce1
unknown <polibos6366@gmail.com>
2024-08-26 17:19:55 -0600

    top level await

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -14,3 +14,9 @@
 // import add from "./shoppingCart.js";
 
 // add("pizza", 2);
+
+// top level await only works with modules
+const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+const data = await res.json();
+console.log(data);
