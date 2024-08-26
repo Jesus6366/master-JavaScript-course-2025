24db48b84c22ff5cae6b7d63418cb28162ae353a
unknown <polibos6366@gmail.com>
2024-08-26 17:04:26 -0600

    exporting and importing es6 modules

---
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -0,0 +1,16 @@
+// importing module name imported
+// import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
+
+// addToCart("bread", 5);
+
+// console.log(price, tq);
+// console.log("Importing module");
+
+// import * as shoppinCart from "./shoppingCart.js";
+
+// console.log(shoppinCart.addToCart("bread", 5));
+// console.log(shoppinCart.totalPrice);
+
+// import add from "./shoppingCart.js";
+
+// add("pizza", 2);
