fe6cc69e06b5e75d9306c49c4191be33f39f2501
unknown <polibos6366@gmail.com>
2024-08-26 17:28:10 -0600

    module pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,6 +1,8 @@
 // importing module name imported
 // import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
 
+import { addToCart } from "./shoppingCart";
+
 // addToCart("bread", 5);
 
 // console.log(price, tq);
@@ -16,7 +18,31 @@
 // add("pizza", 2);
 
 // top level await only works with modules
-const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+// const res = await fetch("https://jsonplaceholder.typicode.com/posts");
 
-const data = await res.json();
-console.log(data);
+// const data = await res.json();
+// console.log(data);
+
+/////////////////MODULE PATTERN
+// ifee
+
+// const shoppingCart2 = (function () {
+//   const cart = [];
+//   const shoppinCart = 10;
+//   const totalPrice = 237;
+//   const totalQuantity = 23;
+//   const addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(`${quantity} ${product} order to supplier`);
+//   };
+
+//   // public api
+//   return {
+//     addToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// shoppingCart2.addToCart("apple", 4);
